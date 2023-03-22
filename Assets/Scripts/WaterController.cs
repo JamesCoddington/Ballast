cc305bc902c920dcf32d25689535b516a71d2bb7
James Coddington <69975261+JamesCoddington@users.noreply.github.com>
2023-03-22 16:34:36 -0500

    Change to MoveTowards

--- Assets/Scripts/WaterController.cs
+++ Assets/Scripts/WaterController.cs
@@ -14,6 +14,7 @@ public class WaterController : MonoBehaviour
     public float cracks;
     Water water = Water.Falling;
     float t;
+    // 15 Is good
     public float duration;
     Vector3 startPosition;
     Vector3 target;
@@ -23,12 +24,10 @@ public class WaterController : MonoBehaviour
     {
         startPosition = transform.position;
         target = (transform.position + new Vector3(0f, 2.5f, 0f));
-        //duration = 60f;
         t = 0f;
-        //StartCoroutine(LerpWater());
     }
 
-    void Update()
+    void LateUpdate()
     {
         if (cracks > 0)
         {
@@ -37,9 +36,10 @@ public class WaterController : MonoBehaviour
                 water = Water.Rising;
                 t = 0f;
             }
-            float cracksTime = duration / cracks;
-            t += Time.deltaTime / cracksTime;
-            transform.position = Vector3.Lerp(transform.position, target, t);
+            //float cracksTime = duration / cracks;
+            //t += Time.deltaTime / cracksTime;
+            //transform.position = Vector3.Lerp(transform.position, target, t);
+            transform.position = Vector3.MoveTowards(transform.position, target, cracks * (Time.deltaTime / duration));
         }
         else
         {
@@ -48,74 +48,9 @@ public class WaterController : MonoBehaviour
                 water = Water.Falling;
                 t = 0f;
             }
-            t += Time.deltaTime / duration;
-            transform.position = Vector3.Lerp(transform.position, startPosition, t);
-        }
-        //if (cracks > 0)
-        //{
-        //    RaiseWater();
-        //} else if (cracks < 1)
-        //{
-        //    LowerWater();
-        //}
-    }
-
-    void RaiseWater()
-    {
-        t = 0f;
-        float cracksTime = duration / cracks;
-        while (cracks > 0)
-        {
-            t += Time.deltaTime / cracksTime;
-            transform.position = Vector3.Lerp(startPosition, target, t);
+            //t += Time.deltaTime / duration;
+            //transform.position = Vector3.Lerp(transform.position, startPosition, t);
+            transform.position = Vector3.MoveTowards(transform.position, startPosition, (Time.deltaTime / duration));
         }
     }
-
-    void LowerWater()
-    {
-        t = 0f;
-        while (cracks < 1)
-        {
-            t += Time.deltaTime / duration;
-            transform.position = Vector3.Lerp(transform.position, startPosition, t);
-        }
-    }
-
-    //IEnumerator LerpPosition(Vector3 targetPosition, float duration)
-    //{
-    //    float time = 0;
-    //    Vector3 startPosition = transform.position;
-    //    while (time < duration && cracks > 0)
-    //    {
-    //        transform.position = Vector3.Lerp(startPosition, targetPosition, time / duration);
-    //        time += Time.deltaTime;
-    //        yield return null;
-    //    }
-    //    transform.position = targetPosition;
-    //}
-
-    //IEnumerator LerpWater()
-    //{
-    //    while (true)
-    //    {
-    //        t = 0f;
-    //        while (cracks < 1)
-    //        {
-    //            t += Time.deltaTime / duration;
-    //            transform.position = Vector3.Lerp(transform.position, startPosition, t);
-    //        }
-    //        t = 0f;
-    //        while (cracks > 0 && transform.position == target)
-    //        {
-    //            float cracksTime = duration / cracks;
-    //            t += Time.deltaTime / cracksTime;
-    //            transform.position = Vector3.Lerp(startPosition, target, t);
-    //        }
-    //        if (transform.position == target)
-    //        {
-    //            print("Game Over");
-    //        }
-    //    }
-    //}
-
 }

dc9aed1286295f0e3a1cdc632f0971b82dbb29c4
James Coddington <69975261+JamesCoddington@users.noreply.github.com>
2023-05-01 17:00:44 -0500

    Sonar (#15)
    * Added basic sonar
    
    * working minimap
    
    * Added sonar movement and screen
    
    * Sonar able to detect collisions
    
    * Created new scene to bypass merge conflict
    
    * Sonar Hotfix
    
    * Sonar Animation

---
+++ Assets/Scripts/Radar.cs
@@ -0,0 +1,47 @@
+﻿/* 
+    ------------------- Code Monkey -------------------
+    
+    Thank you for downloading the Code Monkey Utilities
+    I hope you find them useful in your projects
+    If you have any questions use the contact form
+    Cheers!
+
+               unitycodemonkey.com
+    --------------------------------------------------
+ */
+ 
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using CodeMonkey.Utils;
+using CodeMonkey;
+
+public class Radar : MonoBehaviour {
+
+    [SerializeField] private Transform pfRadarPing;
+    [SerializeField] private LayerMask radarLayerMask;
+
+    private Transform sweepTransform;
+    private float rotationSpeed;
+    private float radarDistance;
+    private List<Collider2D> colliderList;
+
+    private void Awake() {
+        sweepTransform = transform.Find("Sweep");
+        rotationSpeed = 180f;
+        radarDistance = 150f;
+        colliderList = new List<Collider2D>();
+    }
+
+    private void Update() {
+        float previousRotation = (sweepTransform.eulerAngles.z % 360) - 180;
+        sweepTransform.eulerAngles -= new Vector3(0, 0, rotationSpeed * Time.deltaTime);
+        float currentRotation = (sweepTransform.eulerAngles.z % 360) - 180;
+
+        if (previousRotation < 0 && currentRotation >= 0) {
+            // Half rotation
+            colliderList.Clear();
+        }
+    }
+
+}
---
+++ Assets/Scripts/RadarPing.cs
@@ -0,0 +1,25 @@
+﻿/* 
+    ------------------- Code Monkey -------------------
+    
+    Thank you for downloading the Code Monkey Utilities
+    I hope you find them useful in your projects
+    If you have any questions use the contact form
+    Cheers!
+
+               unitycodemonkey.com
+    --------------------------------------------------
+ */
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RadarPing : MonoBehaviour {
+    private void OnTriggerEnter(Collider other)
+    {
+        //Debug.Log("Hit2");
+        Debug.Log("Hit2 " + other.gameObject.GetComponent<Animation>());
+        other.gameObject.GetComponent<Animation>().Play("SonarPip");
+    }
+}
